ec2fa52a4af6db0bc31aeea52c961159f6368013
Cristyan Resende <cristyan.souza@al.infnet.edu.br>
2025-05-16 23:08:04 -0300

    exercicio 3

--- Pages/CityManager/CreateCity.cshtml.cs
+++ Pages/CityManager/CreateCity.cshtml.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,21 +6,28 @@ namespace cristyan_resende_DR4_TP2.Pages.CityManager;
 
 public class CreateCityModel : PageModel
 {
-    public string Message;
+    public string Message { get; set; }
+
+    public class InputModel
+    {
+        [Required(ErrorMessage = "O nome da cidade é obrigatório.")]
+        [MinLength(3, ErrorMessage = "O nome da cidade deve ter no mínimo 3 caracteres.")]
+        public string CityName { get; set; }
+    }
+
+    [BindProperty]
+    public InputModel City { get; set; }
+
     public void OnGet()
     {
-        Message = "Cadastre uma cidade turística: ";
+        Message = "Cadastre uma cidade turística.";
     }
-    public void OnPost(string CityName)
-    {
-        if (!string.IsNullOrEmpty(CityName))
-        {
-            Message = $"A cidade cadastrada é: {CityName}.";
-        }
-        else
-        {
-            Message = "Insira o nome de uma cidade:";
 
+    public void OnPost()
+    {
+        if (ModelState.IsValid)
+        {
+            Message = $"Cidade cadastrada: {City.CityName}";
         }
     }
 }
