ebd2b50387e7223281623ec347061691c3dd5833
Cristyan Resende <cristyan.souza@al.infnet.edu.br>
2025-05-17 01:02:51 -0300

    11 - Manipulação de Arquivos: Leitura

--- Pages/SaveNote.cshtml.cs
+++ Pages/SaveNote.cshtml.cs
@@ -8,6 +8,8 @@ public class SaveNoteModel : PageModel
 {
     public string Message { get; set; }
     public string FilePath { get; set; }
+    public string FileContent { get; set; }
+    public List<string> Files { get; set; } = new();
 
     public class InputModel
     {
@@ -20,6 +22,7 @@ public class SaveNoteModel : PageModel
 
     public void OnGet()
     {
+        LoadFiles();
         Message = "Digite o conteúdo da nota e salve:";
     }
 
@@ -42,6 +45,7 @@ public class SaveNoteModel : PageModel
 
                 Message = "Nota salva com sucesso!";
                 FilePath = $"/files/{fileName}";
+                LoadFiles();
             }
             catch (Exception ex)
             {
@@ -51,6 +55,34 @@ public class SaveNoteModel : PageModel
         else
         {
             Message = "Por favor, digite algum conteúdo.";
+        }
+    }
+
+    public void OnGetView(string fileName)
+    {
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", fileName);
+
+        if (System.IO.File.Exists(filePath))
+        {
+            FileContent = System.IO.File.ReadAllText(filePath);
+            Message = $"Conteúdo do arquivo: {fileName}";
+        }
+        else
+        {
+            Message = "Arquivo não encontrado.";
+        }
+
+        LoadFiles();
+    }
+
+    private void LoadFiles()
+    {
+        var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files");
+        if (Directory.Exists(folderPath))
+        {
+            Files = Directory.GetFiles(folderPath, "*.txt")
+                             .Select(Path.GetFileName)
+                             .ToList();
         }
     }
 }
