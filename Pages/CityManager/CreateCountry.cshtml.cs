cbdc2310fa7dc24ee3f9b823b4610ce6707ebb12
Cristyan Resende <cristyan.souza@al.infnet.edu.br>
2025-05-16 23:45:12 -0300

    5 Encapsulando Entrada em Objeto Complexo

---
+++ Pages/CityManager/CreateCountry.cshtml.cs
@@ -0,0 +1,55 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cristyan_resende_DR4_TP2.Pages.CityManager
+{
+    public class CreateCountryModel : PageModel
+    {
+        public string Message { get; set; }
+        public class Country()
+        {
+            public string CountryName { get; set; }
+            public string CountryCode { get; set; }
+        }
+        public class InputModel
+        {
+            [BindProperty]
+            [Required(ErrorMessage = "O nome do país é obrigatório.")]
+            public string CountryName { get; set; }
+
+            [BindProperty]
+            [Required(ErrorMessage = "O código do país é obrigatório.")]
+            [StringLength(3, ErrorMessage = "O código do país deve ter no máximo 3 caracteres.")]
+            public string CountryCode { get; set; }
+        }
+
+        [BindProperty]
+        public InputModel CountryInput { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Cadastre um país:";
+        }
+
+        public void OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                // Criando uma instância de Country com os dados do InputModel
+                var country = new Country
+                {
+                    CountryName = CountryInput.CountryName,
+                    CountryCode = CountryInput.CountryCode
+                };
+
+                Message = $"País cadastrado: {country.CountryName} ({country.CountryCode})";
+            }
+            else
+            {
+                Message = "Erro no cadastro. Verifique os campos.";
+            }
+        }
+    }
+
+}
