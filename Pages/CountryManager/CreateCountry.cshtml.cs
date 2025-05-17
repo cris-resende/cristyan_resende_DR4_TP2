2bef706eb40e6a6a5d422a742b0ecaa2db4d6d6e
Cristyan Resende <cristyan.souza@al.infnet.edu.br>
2025-05-17 01:06:33 -0300

    12 - Validação Customizada com ModelState

--- Pages/CountryManager/CreateCountry.cshtml.cs
+++ Pages/CountryManager/CreateCountry.cshtml.cs
@@ -2,54 +2,46 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace cristyan_resende_DR4_TP2.Pages.CountryManager;
+namespace cristyan_resende_DR4_TP2.Pages;
 
 public class CreateCountryModel : PageModel
 {
     public string Message { get; set; }
 
-    // Classe para representar um país
-    public class Country
-    {
-        public string CountryName { get; set; }
-        public string CountryCode { get; set; }
-    }
-
-    // Classe de entrada para capturar os dados do formulário
     public class InputModel
     {
+        [BindProperty]
         [Required(ErrorMessage = "O nome do país é obrigatório.")]
+        [MinLength(3, ErrorMessage = "O nome do país deve ter no mínimo 3 caracteres.")]
         public string CountryName { get; set; }
 
+        [BindProperty]
         [Required(ErrorMessage = "O código do país é obrigatório.")]
         [StringLength(2, MinimumLength = 2, ErrorMessage = "O código do país deve ter exatamente 2 caracteres.")]
         public string CountryCode { get; set; }
     }
 
-    // Lista para armazenar múltiplos países
     [BindProperty]
-    public List<InputModel> Countries { get; set; } = new List<InputModel>();
+    public InputModel Country { get; set; }
 
     public void OnGet()
     {
-        // Inicializa a lista com 5 registros vazios
-        for (int i = 0; i < 5; i++)
-        {
-            Countries.Add(new InputModel());
-        }
-        Message = "Cadastre até 5 países simultaneamente:";
+        Message = "Cadastre um país:";
     }
 
     public void OnPost()
     {
+        if (!string.IsNullOrEmpty(Country.CountryName) && !string.IsNullOrEmpty(Country.CountryCode))
+        {
+            if (char.ToUpper(Country.CountryName[0]) != char.ToUpper(Country.CountryCode[0]))
+            {
+                ModelState.AddModelError("Country.CountryCode", "O código do país deve começar com a mesma letra que o nome.");
+            }
+        }
+
         if (ModelState.IsValid)
         {
-            // Monta a lista de países cadastrados
-            var countryList = Countries
-                .Where(c => !string.IsNullOrEmpty(c.CountryName))
-                .Select(c => $"{c.CountryName} ({c.CountryCode})");
-
-            Message = "Países cadastrados: " + string.Join(", ", countryList);
+            Message = $"País cadastrado: {Country.CountryName} ({Country.CountryCode})";
         }
         else
         {
